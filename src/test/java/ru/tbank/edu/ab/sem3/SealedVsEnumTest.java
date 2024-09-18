853aee449a93a8e22cc96e006d810e889c99c3c5
s.a.khvatov <s.a.khvatov@tinkoff.ru>
2024-09-18 13:08:52 +0300

    Перечисления vs Sealed-классы

---
+++ src/test/java/ru/tbank/edu/ab/sem3/SealedVsEnumTest.java
@@ -0,0 +1,37 @@
+package ru.tbank.edu.ab.sem3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.tbank.edu.ab.sem3.SealedVsEnum.EnumExample;
+import ru.tbank.edu.ab.sem3.SealedVsEnum.SealedExample;
+
+
+class SealedVsEnumTest {
+
+    @Test
+    @DisplayName("Работа с sealed-классами")
+    void sealedClassTest() {
+        var color = SealedExample.Color.fromRgb("#FF0000")
+                .orElseThrow(IllegalArgumentException::new);
+        var description = switch (color) {
+            case SealedExample.Red ignored -> "Красный";
+            case SealedExample.Blue ignored -> "Синий";
+            case SealedExample.Green ignored -> "Зеленый";
+        };
+        System.out.printf("%s - это %s цвет%n", color.getRgb(), description);
+    }
+
+    @Test
+    @DisplayName("Работа с перечислениями")
+    void enumTest() {
+        var color = EnumExample.Color.fromRgb("#FF0000")
+                .orElseThrow(IllegalArgumentException::new);
+        var description = switch (color) {
+            case EnumExample.Color.RED -> "Красный";
+            case EnumExample.Color.BLUE -> "Синий";
+            case EnumExample.Color.GREEN -> "Зеленый";
+        };
+        System.out.printf("%s - это %s цвет%n", color.getRgb(), description);
+    }
+
+}
