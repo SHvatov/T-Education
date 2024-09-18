bdd5011b576df217f66eb6a2a85227aa8925ce5b
s.a.khvatov <s.a.khvatov@tinkoff.ru>
2024-09-18 16:42:04 +0300

    Перечисления vs Sealed-классы

--- src/main/java/ru/tbank/edu/ab/sem3/Person.java
+++ src/main/java/ru/tbank/edu/ab/sem3/Person.java
@@ -1,10 +1,17 @@
 package ru.tbank.edu.ab.sem3;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.With;
 
+
+@With
 @Data
+@Builder
 public class Person {
+
     private final String name;
     private final String surname;
     private final int age;
+
 }
